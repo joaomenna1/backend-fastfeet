24bf7c346a6ba189077008f58e11e1a7dbb78fae
joao nogueira <joaomenna1@gmail.com>
2020-03-13 13:36:50 -0400

    feature: methods password hash, checkpass

--- src/app/Models/User.js
+++ src/app/Models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,14 +7,33 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       {
         sequelize,
       }
     );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
     return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 
 export default User;
+
+/* --> Utiliza-se addHook para antes de armazenar a senha
+       realizar o hash.
+
+  --> Utiliza-se o metodo checkPassword para comparar a senha
+      em que o usuário entrou e com a senha armazenada no banco.
+*/
