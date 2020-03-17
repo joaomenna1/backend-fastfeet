f2b4b28cb730222101d827aa533b610833f30486
joao nogueira <joaomenna1@gmail.com>
2020-03-17 15:14:27 -0400

    feature: update recipients

--- src/app/Controllers/RecipientController.js
+++ src/app/Controllers/RecipientController.js
@@ -48,6 +48,46 @@ class RecipientController {
       cep,
     });
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      cep: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.json({ error: 'Validations fails' });
+    }
+
+    const RecipientId = req.userId;
+
+    const RecipientExist = await Recipient.findByPk(RecipientId);
+
+    const {
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    } = await Recipient.update(req.body);
+
+    return res.json({
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    });
+  }
 }
 
 export default new RecipientController();
