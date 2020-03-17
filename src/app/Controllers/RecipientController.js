da8d0c6c63a972103a1915a2766a89cf3c5b8e4e
joao nogueira <joaomenna1@gmail.com>
2020-03-17 15:03:02 -0400

    feature: Recipients create

---
+++ src/app/Controllers/RecipientController.js
@@ -0,0 +1,53 @@
+import * as Yup from 'yup';
+
+import Recipient from '../Models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      cep: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.json({ error: 'Validations fails' });
+    }
+
+    const recipientExist = await Recipient.findOne({
+      where: { name: req.body.name },
+    });
+
+    if (recipientExist) {
+      return res.status(400).json({ error: 'recipient already exist!' });
+    }
+
+    const {
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    } = await Recipient.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    });
+  }
+}
+
+export default new RecipientController();
