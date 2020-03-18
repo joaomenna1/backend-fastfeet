3ec2b84d99eacfe5f8d49559e4a5cf230a02215f
joao nogueira <joaomenna1@gmail.com>
2020-03-18 11:14:39 -0400

    chore: add routes

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,29 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 
 import UserController from './app/Controllers/UserController';
 import SessionsController from './app/Controllers/SessionsController';
 import RecipientController from './app/Controllers/RecipientController';
+import FileController from './app/Controllers/FileController';
 
 import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
-routes.post('/sessions', SessionsController.store);
+/* User admin */
 routes.post('/user', authMiddleware, UserController.store);
+/* sessions */
+routes.post('/sessions', SessionsController.store);
 
+/* routes.use(authMiddleware); */
 /* Routes recipients */
 routes.post('/recipients/', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+
+/* Files */
+routes.post('/files', upload.single('files'), FileController.store);
 
 export default routes;
