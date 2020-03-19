115b270a5327293bdc4d7ff27c19d11f38f61af8
joao nogueira <joaomenna1@gmail.com>
2020-03-19 13:48:04 -0400

    chore: add models

--- src/database/index.js
+++ src/database/index.js
@@ -3,11 +3,13 @@ import Sequelize from 'sequelize';
 import User from '../app/Models/User';
 import Recipient from '../app/Models/Recipient';
 import File from '../app/Models/File';
+import Signature from '../app/Models/Signature';
 import Deliveryman from '../app/Models/Deliverymen';
+import Order from '../app/Models/Order';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient, File, Deliveryman];
+const models = [User, Recipient, File, Signature, Deliveryman, Order];
 
 class Database {
   constructor() {
