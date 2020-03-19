2cb06b7dd7f94a4afc97e753395d31f057898d5b
joao nogueira <joaomenna1@gmail.com>
2020-03-19 13:48:14 -0400

    minor changes

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/Controllers/UserController';
 import SessionsController from './app/Controllers/SessionsController';
 import RecipientController from './app/Controllers/RecipientController';
 import FileController from './app/Controllers/FileController';
+import SignatureController from './app/Controllers/SignatureController';
 import DeliverymanController from './app/Controllers/DeliverymanController';
 
 import authMiddleware from './app/middleware/auth';
@@ -21,19 +22,21 @@ routes.post('/sessions', SessionsController.store);
 
 routes.use(authMiddleware);
 
-/* Routes recipients */
+/* Routes recipients management (Destinatários) */
 routes.post('/recipients/', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
 
-/* Routes Deliveryman */
-
+/* Routes Delivery management (Entregadores) */
 routes.post('/couriers', DeliverymanController.store);
 routes.get('/couriers', DeliverymanController.index);
 routes.put('/couriers/:id', DeliverymanController.update);
 routes.delete('/couriers/:id', DeliverymanController.destroy);
 
-/* Files */
+/* Routes Files (avatars) */
 routes.post('/files', upload.single('files'), FileController.store);
 routes.get('/files', FileController.index);
+
+/* Routes Signatures (assinaturas do destinatários) */
+routes.post('/signature', upload.single('sign'), SignatureController.store);
 
 export default routes;
